cd68e916bf5b00cf746a23554646d9fbf8a1a971
Kai Li <kal182@ucsd.edu>
2019-07-02 17:02:46 -0700

    kl - Added a create action for images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,4 +4,19 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.valid?
+      @image.save
+      redirect_to @image
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -18,4 +18,27 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'form input[name="image[url]"]'
     assert_select 'form input[type=submit]'
   end
+
+  test 'Should create a new image with valid input' do
+    assert_difference('Image.count', 1) do
+      post images_path, params: { image: { url: 'https://abc' } }
+    end
+    assert_redirected_to image_path(Image.last)
+  end
+
+  test 'Should not create a new image with invalid input' do
+    assert_no_difference('Image.count') do
+      post images_path, params: { image: { url: 'abcd' } }
+    end
+    assert_response :unprocessable_entity
+    assert_select 'form input[name="image[url]"]'
+  end
+
+  test 'Should not create a new image with empty url' do
+    assert_no_difference('Image.count') do
+      post images_path, params: { image: { url: '' } }
+    end
+    assert_response :unprocessable_entity
+    assert_select 'form input[name="image[url]"]'
+  end
 end
