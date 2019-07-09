a3d52f1b8e82e67efe2936b90f5a8c47856ca870
Kai Li <kal182@ucsd.edu>
2019-07-09 15:33:58 -0700

    kl - click on button submits feedback to backend and page displays returned message

--- app/javascript/image_sharing/components/CommentsForm.js
+++ app/javascript/image_sharing/components/CommentsForm.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
+import { post } from '../utils/helper';
 
 class CommentsForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      comments: '',
+      failed: false,
+      submissionMessage: ''
+    };
+  }
+
+  onNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
+  onCommentsChange = (e) => {
+    this.setState({ comments: e.target.value });
+  }
+
+  submitFeedback = () => {
+    const promise = post('/api/feedbacks', {
+      name: this.state.name,
+      comments: this.state.comments
+    });
+
+    return promise
+      .then(response => this.setState({ failed: false, submissionMessage: response.message }))
+      .catch(error => this.setState({ failed: true, submissionMessage: error.data.message }))
+      .then(() => this.setState({ name: '', comments: '' }));
+  }
+
   render() {
     return (
       <Form>
+        {this.state.submissionMessage !== '' &&
+          <Alert color={this.state.failed ? 'danger' : 'success'}>
+            {this.state.submissionMessage}
+          </Alert>}
         <FormGroup>
           <Label className="l">Your name:</Label>
-          <Input type="text" name="name" />
+          <Input type="text" name="name" value={this.state.name} onChange={this.onNameChange} />
         </FormGroup>
         <FormGroup>
           <Label>Comments:</Label>
-          <Input type="textarea" name="comments" />
+          <Input
+            type="textarea"
+            name="comments"
+            value={this.state.comments}
+            onChange={this.onCommentsChange}
+          />
         </FormGroup>
-        <Button color="primary">Submit</Button>
+        <Button color="primary" onClick={this.submitFeedback}>Submit</Button>
       </Form>
     );
   }
--- app/javascript/image_sharing/test/components/CommentsForm.spec.js
+++ app/javascript/image_sharing/test/components/CommentsForm.spec.js
@@ -3,11 +3,21 @@
 import assert from 'assert';
 import { shallow } from 'enzyme';
 import React from 'react';
+import sinon from 'sinon';
+import * as api from '../../utils/helper';
 import CommentsForm from '../../components/CommentsForm';
 
 describe('<CommentsForm />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<CommentsForm />);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it('should render correctly', () => {
-    const wrapper = shallow(<CommentsForm />);
     const groups = wrapper.find('FormGroup');
 
     assert.strictEqual(groups.length, 2);
@@ -24,5 +34,38 @@ describe('<CommentsForm />', () => {
 
     assert.strictEqual(wrapper.find('Button').length, 1);
     assert.strictEqual(wrapper.find('Button').children().text(), 'Submit');
+
+    assert.strictEqual(wrapper.find('Alert').length, 0);
+
+    assert.deepStrictEqual(wrapper.instance().state, {
+      name: '',
+      comments: '',
+      failed: false,
+      submissionMessage: ''
+    });
+  });
+
+  it('should show error when submission fails', () => {
+    sinon.stub(api, 'post').rejects({ data: { message: 'failed' } });
+
+    return wrapper.instance().submitFeedback().then(() => {
+      const alert = wrapper.find('Alert');
+
+      assert.strictEqual(alert.length, 1);
+      assert.strictEqual(alert.props().children, 'failed');
+      assert.strictEqual(alert.props().color, 'danger');
+    });
+  });
+
+  it('should show succeeded when submission succeeded', () => {
+    sinon.stub(api, 'post').resolves({ message: 'succeeded' });
+
+    return wrapper.instance().submitFeedback().then(() => {
+      const alert = wrapper.find('Alert');
+
+      assert.strictEqual(alert.length, 1);
+      assert.strictEqual(alert.props().children, 'succeeded');
+      assert.strictEqual(alert.props().color, 'success');
+    });
   });
 });
